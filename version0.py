7d99041439b9746c5d3ac7b8416bcd0a3e9dee78
Tavares <127125922+GTavrss@users.noreply.github.com>
2023-05-08 13:48:27 -0300

    Update version0.py
    Corrected the overlap check and optimized a little the find_next_time function

--- version0.py
+++ version0.py
@@ -50,6 +50,7 @@ def get_week(relative_week=0):
 
 
 class Week():
+    
     def __init__(self, week_start,week_end):
         self.week_start = week_start
         self.week_end = week_end
@@ -57,6 +58,9 @@ class Week():
         self.events = []
         self.full = False
         self.refresh_events()
+        
+        
+        
     def refresh_events(self):
         self.events = []
         self.week_days = []
@@ -64,12 +68,19 @@ class Week():
             self.week_days.append(Day(i,9,22))
         for i in self.week_days:
             self.events.extend(i.events)
+            
+            
+            
     def find_next_time(self, event):
         time = []
         scheduled = False
         i = 1
+        
+        #Definir para o primeiro dia
+        #Checar se tem overlap
+        #Se tiver -> projected_start = end_time
         while (not scheduled) and i<=5:
-            #para cada dia útil da semana
+            # para cada dia útil da semana
             self.refresh_events()
             day = self.week_days[i]
             start = day.start_time
@@ -78,22 +89,26 @@ class Week():
             d = pd.to_datetime(day.date)
             d_ = d.date()
             SPTz = pytz.timezone("America/Sao_Paulo") 
-            end_ = datetime.datetime(d_.year,d_.month,d_.day,end,0,0,0,SPTz)
-            projected_start = datetime.datetime(d_.year,d_.month,d_.day,start,0,0,0,SPTz)
+            end_ = datetime.datetime(d_.year,d_.month,d_.day,end,0,0,0,SPTz) - pd.to_timedelta('6 min')
+            projected_start = datetime.datetime(d_.year,d_.month,d_.day,start,0,0,0,SPTz) - pd.to_timedelta('6 min')
             while projected_start.replace(tzinfo=None) < end_.replace(tzinfo=None):
                 valid = True
                 projected_end = projected_start + pd.to_timedelta(f"{event.duration} min") - pd.to_timedelta('1 min')
                 #-------Check for overlap --------------
                 for ev in events:
-                    if not scheduled:
-                        start_time = ev.start_event
-                        end_time = ev.end_event
-                        start_time = pd.to_datetime(start_time)
-                        end_time = pd.to_datetime(end_time) - pd.to_timedelta('1 min')
-                        
-                        if (projected_start.replace(tzinfo=None) >= start_time.replace(tzinfo=None) and projected_start.replace(tzinfo=None) <= end_time.replace(tzinfo=None)) or(
-                                projected_end.replace(tzinfo=None) >= start_time.replace(tzinfo=None) and projected_end.replace(tzinfo=None) <= end_time.replace(tzinfo=None)):
-                            valid = False
+                    if not pd.to_datetime(ev.start_event).replace(tzinfo=None) > projected_end.replace(tzinfo=None):
+                        if not scheduled:
+                            start_time = ev.start_event
+                            end_time = ev.end_event
+                            start_time = pd.to_datetime(start_time)
+                            end_time = pd.to_datetime(end_time) - pd.to_timedelta('1 min')
+                            
+                            if (projected_start.replace(tzinfo=None) >= start_time.replace(tzinfo=None) and projected_start.replace(tzinfo=None) <= end_time.replace(tzinfo=None)) or(
+                                    projected_end.replace(tzinfo=None) >= start_time.replace(tzinfo=None) and projected_end.replace(tzinfo=None) <= end_time.replace(tzinfo=None)) or(
+                                    projected_start.replace(tzinfo=None) <= start_time.replace(tzinfo=None) and projected_end.replace(tzinfo=None) >= end_time.replace(tzinfo=None)    ):
+                                valid = False
+                                projected_start = end_time + pd.to_timedelta('1 min')
+
                 #------------------------------------
                 if valid:
                     if not event.daily and not scheduled:
@@ -102,19 +117,21 @@ class Week():
                         break
                     elif event.daily:
                         time.append(projected_start)
+
                         if i == 5: scheduled=True
                         break
-                projected_start = projected_start+ pd.to_timedelta('15 min')
             i = i+1
         if len(time)>0: return time
         else: return None   
+
+
 
     def create_event(self, event):
         
         if not event.scheduled:
             times = self.find_next_time(event)
             for t in times:
-                t = pd.to_datetime(t) - pd.to_timedelta('6 min')
+                t = pd.to_datetime(t) #- pd.to_timedelta('6 min')
                 body_event = {
                         "summary": f"{event.event_name}",
                         "description": event.description + " __created_in_python__.",
@@ -133,14 +150,18 @@ class Week():
                 service = build('calendar', 'v3', credentials=get_credentials())         
                 schedule = service.events().insert(calendarId=calendar_Id, body=body_event).execute()       
                 event.id.append(schedule['id'])
-                event.scheduled = True     
+                event.scheduled = True 
+                self.refresh_events()
+                
+            
+            
     def delete_all_created_events(self):
         self.refresh_events()
         for e in self.events:
             if 'description' in e.event_dic.keys():
                 if "__created_in_python__"  in e.event_dic['description']:
                     e.delete_event()
-      
+          
 class Day():
     def __init__(self,date,start_time,end_time):
         self.date = date
